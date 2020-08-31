8a3623f39ec07506019e43aff2403ee5990878ca
Jane Greene <janegreenecasa@gmail.com>
2020-08-31 11:17:42 -0600

    customize the attributes with the gem

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,10 @@
 class OrderSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :order_number
+  has_many :items # now you will see the relationships in the response
+
+  attribute :item_count do |object| #customer attribute see fastjson api docs 
+    object.items.size
+  end
+
 end
